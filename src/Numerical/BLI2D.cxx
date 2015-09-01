b7943bed61aa57879724f6aea5f446e4185ac6e6
Gabriel Perdue <perdue@fnal.gov>
2015-09-01 13:14:32 -0500

    GENIE 2.9.0 based on HepForge SVN rev 5475

--- src/Numerical/BLI2D.cxx
+++ src/Numerical/BLI2D.cxx
@@ -24,6 +24,7 @@
 #include <TMath.h>
 
 #include <cassert>
+#include <limits>
 
 #include "Messenger/Messenger.h"
 #include "Numerical/BLI2D.h"
@@ -91,7 +92,7 @@ bool BLI2DUnifGrid::AddPoint(double x, double y, double z)
   fZ[iz] = z;
 
   fZmin = TMath::Min(z, fZmin);
-  fZmax = TMath::Min(z, fZmax);
+  fZmax = TMath::Max(z, fZmax);
 
   LOG("BLI2DUnifGrid", pDEBUG) 
     << "Added x = " << x << " (ix = " << ix << ")"
@@ -149,8 +150,8 @@ void BLI2DUnifGrid::Init(
   fXmax = 0.;
   fYmin = 0.;
   fYmax = 0.;
-  fZmin = 0.;
-  fZmax = 0.;
+  fZmin = std::numeric_limits<double>::max();
+  fZmax = std::numeric_limits<double>::min();
   fDX   = 0.;
   fDY   = 0.;
   fX    = 0;
@@ -166,8 +167,8 @@ void BLI2DUnifGrid::Init(
     fXmax = xmax;
     fYmin = ymin;
     fYmax = ymax;
-    fZmin = 0.;
-    fZmax = 0.;
+    fZmin = std::numeric_limits<double>::max();
+    fZmax = std::numeric_limits<double>::min();
 
     fDX = (xmax-xmin)/(nx-1);
     fDY = (ymax-ymin)/(ny-1);
@@ -324,7 +325,7 @@ bool BLI2DNonUnifGrid::AddPoint(double x, double y, double z)
   fZ[iz] = z;
 
   fZmin = TMath::Min(z, fZmin);
-  fZmax = TMath::Min(z, fZmax);
+  fZmax = TMath::Max(z, fZmax);
 
   LOG("BLI2DNonUnifGrid", pDEBUG) 
     << "Added x = " << x << " (ix = " << xidx << ")"
@@ -408,8 +409,8 @@ void BLI2DNonUnifGrid::Init(
   fXmax  = 0.;
   fYmin  = 0.;
   fYmax  = 0.;
-  fZmin  = 0.;
-  fZmax  = 0.;
+  fZmin  = std::numeric_limits<double>::max();
+  fZmax  = std::numeric_limits<double>::min();
   fX     = 0;
   fY     = 0;
   fZ     = 0;
@@ -423,8 +424,8 @@ void BLI2DNonUnifGrid::Init(
     fXmax = xmax;
     fYmin = ymin;
     fYmax = ymax;
-    fZmin = 0.;
-    fZmax = 0.;
+    fZmin = std::numeric_limits<double>::max();
+    fZmax = std::numeric_limits<double>::min();
 
     fX = new double[fNX];
     fY = new double[fNY];
